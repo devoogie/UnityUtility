a57a9ceb5f2eef0ea5ac820ea051048207dcd5ed
devboogie <goboogie@icloud.com>
2022-03-04 12:13:31 +0900

    카메라 수정

--- Scripts/Code/Util/CameraManager.cs
+++ Scripts/Code/Util/CameraManager.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoSingleton<CameraManager>,IManager
+public class CameraManager : MonoSingleton<CameraManager>, IManager
 {
-    public static Vector3 TargetPosition;
+    public CameraSetting setting = new CameraSetting();
+    public static CameraSetting Setting => Instance.setting;
     public override void Initialize()
     {
     }
@@ -12,21 +13,22 @@ public class CameraManager : MonoSingleton<CameraManager>,IManager
     /// LateUpdate is called every frame, if the Behaviour is enabled.
     /// It is called after all Update functions have been called.
     /// </summary>
-    
+
 
     public static Vector3 GetPositionWorldToRect(Vector3 world)
     {
-        
+
         return Camera.main.WorldToScreenPoint(world);
     }
 
     [System.Serializable]
-    public static class Setting
+    public class CameraSetting
     {
-        public static float SpeedFollowMove { get; set; } = .5f;
-        public static float CameraZoom { get; set; } = 15f;
-        public static float SpeedZoom { get; set; } = 0.05f;
-        public static float SpeedFollowZoom { get; set; } = 0.05f;
-        public static bool IsFollowZoom { get; set; } = false;
+        public Vector3 TargetPosition;
+        public float SpeedFollowMove = .5f;
+        public float CameraZoom = 15f;
+        public float SpeedZoom = 0.05f;
+        public float SpeedFollowZoom = 0.05f;
+        public bool IsFollowZoom = false;
     }
 }
