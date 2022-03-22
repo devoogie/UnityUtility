cd99a11be1501a575fce9b9d1de183890d31125d
devboogie <goboogie@icloud.com>
2022-03-22 15:51:30 +0900

    Line

---
+++ Scripts/Code/Line.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Line : PoolableMono
+{
+    public LineRenderer lineRenderer;
+    public Material defaultMaterial;
+    public override void OnDespawn()
+    {
+        lineRenderer.loop = false;
+        SetMaterial(defaultMaterial);
+        SetColor(Color.white);
+    }
+    public void SetMaterial(Material material)
+    {
+        lineRenderer.material = material;
+    }
+    public override void OnInitialize()
+    {
+        lineRenderer = GetComponentInChildren<LineRenderer>();
+    }
+
+    public override void OnSpawn()
+    {
+
+    }
+    public void SetPositions(params Vector3[] positions)
+    {
+        lineRenderer.positionCount = positions.Length;
+        lineRenderer.SetPositions(positions);
+    }
+    public void DrawCircle(int step, float radius)
+    {
+        lineRenderer.positionCount = step;
+        lineRenderer.loop = true;
+        var circles = radius.ToCircle(step);
+        var count = 0;
+        foreach (var circle in circles)
+        {
+            var position = circle.ToXZ();
+            position.y = 0.1f;
+            lineRenderer.SetPosition(count,position );
+            count++;
+        }
+        
+    }
+    public void SetColor(Color color)
+    {
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+    }
+}
