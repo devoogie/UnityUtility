43632f7d630e13271cd9bf3fa15fc74ef6dd64a1
devoogie <goboogie@icloud.com>
2022-06-08 16:27:57 +0900

    Color View

--- Scripts/Code/View.cs
+++ Scripts/Code/View.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public abstract class View : UI
+public abstract class UIFullScreen : UI
 {
     public override void OnMonoShow()
     {
