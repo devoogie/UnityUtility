78b8945bc6c6289c15d40db96a9f1fc22104185f
devoogie <goboogie@icloud.com>
2022-06-20 14:52:55 +0900

    feature add expBar color

--- Scripts/Code/Util/ColorSet.cs
+++ Scripts/Code/Util/ColorSet.cs
@@ -14,7 +14,7 @@ public static class ColorSet
     public static readonly Color ClearBlack = "#00000000".ToColor();
     public static readonly Color ClearWhite = "#ffffff00".ToColor();
     public static readonly Color Link = "#75faaaff".ToColor();
-    
+
     public static readonly Color Grade_Normal = "#A5B9CD".ToColor();
     public static readonly Color Grade_Rare = "#3493ff".ToColor();
     public static readonly Color Grade_Epic = "#BE41DC".ToColor();
@@ -39,4 +39,6 @@ public static class ColorSet
 
     public static readonly Color Sky = "#3FB3F1".ToColor();
     public static readonly Color DarkRed = "#B90000".ToColor();
+    public static readonly Color EXP_NORMAL = "#2FE097".ToColor();
+    public static readonly Color EXP_MAX = "#0861DA".ToColor();
 }
